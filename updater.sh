dad450bd0d7bbe83bd873c67e4ae7996de39375c
DinosaurEater <ratanprasanna3@gmail.com>
2023-09-13 15:53:42 +0530

    Updates ClamAV database, seperate file for configuration

--- updater.sh
+++ updater.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
+trap 'echo "INTERRUPTED" | tee -a $log_file; notify-send -t 100 "Update" "Interrupted"; exit 1' SIGINT
 
-trap 'echo "INTERRUPTED" | tee -a $logfile; notify-send -t 100 "Update" "Command Interrupted"; exit 1' SIGINT
-##PREPARING
+#Find the directory of script
 path=`dirname $0`
-logfile="$path/updater.log"
-if [ ! -f $logfile ]; then 
- touch $logfile
- echo "++++logfile CREATED++++"
+
+#Construct path of logfile and configfile
+config_file="$path/updater.cfg"
+log_file="$path/updater.log"
+
+#Create logfile and configfile if not found
+if [ ! -f $log_file ]; then 
+ touch $log_file
+ echo "++++logfile CREATED++++" | tee -a "$log_file"
+fi
+if [ ! -f $config_file ]; then
+ touch $config_file
+ echo "++++Configfile created++++" | tee -a "$log_file"
+ echo "#Set to true to check for AUR updates" >> "$config_file"
+ echo "perform_AUR_Update=true" >> "$config_file"
+ echo " " >> "$config_file"
+ echo "#Set to true to update ClamAV database (recommended)" >> "$config_file"
+ echo "perform_ClamAVdb_update=true" >> "$config_file"
+ echo "CONFIG FILE POPULATED" | tee -a "$log_file"
+ source "$config_file"
 fi
 
 ##LOGGING
-echo "**********************************************$(date) UPDATE****************************************************" >> "$logfile"
-echo "starting $(date)" | tee -a "$logfile"
+echo "**********************************************$(date) UPDATE****************************************************" >> "$log_file"
+echo "starting $(date)" | tee -a "$log_file"
 notify-send -h int:transient:1 -t 100 "Update" "starting.."
 
 ##UPDATING
-sudo pacman -Syu --noconfirm --color=always 2>&1 | tee -a "$logfile"
 
+#System update(PACMAN)
+sudo pacman -Syu --noconfirm --color=always 2>&1 | tee -a "$log_file"
+
+#AUR update(YAY)
+if [ "$perform_AUR_Update" = true ]; then
 notify-send -h int:transient:1 -t 100 "Update" "AUR"
 echo "Searching AUR.."
-echo "---AUR---" >> "$logfile"
+echo "---AUR---" >> "$log_file"
+yay -Syu --noconfirm --color=always 2>&1 | tee -a "$log_file"
+fi
 
-yay -Syu --noconfirm --color=always 2>&1 | tee -a "$logfile"
+#ClamAV database update (IF clamav is installed)
+if [[ "$perform_ClamAVdb_update" = true && $(pacman -Qs clamav) > /dev/null ]]; then
+ notify-send -h int:transient:1 -t 100 "Update" "ClamAV database"
+ echo "ClamAV databse.."
+ echo "++++ClamAV database++++" >> "$log_file"
+ sudo freshclam | tee -a "$log_file"
+fi
 
-##NOTIFYING
+##NOTIFYING after completion
 notify-send -h int:transient:1 "Update" "Command Completed"
 echo "COMPLETED" 
+exit 0
